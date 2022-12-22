4a1fca2172453b843956412320874ee8d73a2515
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2022-12-22 17:08:47 -0300

    Exercício de herança

---
+++ 14 - Herança e polimorfismo/Herança/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.BusinessAcount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BusinessAcount account = new BusinessAcount();
+		
+		
+	}
+
+}

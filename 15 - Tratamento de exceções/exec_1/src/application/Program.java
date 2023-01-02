c6561770ad3b838d2427351a25cd6090d2360afe
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-02 15:17:29 -0300

    Solução (boa) recomendada

--- 15 - Tratamento de exceções/exec_1/src/application/Program.java
+++ 15 - Tratamento de exceções/exec_1/src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Account;
+import exceptions.BusinessException;
 
 public class Program {
 
@@ -29,16 +30,13 @@ public class Program {
 		System.out.println(); // QUEBRA DE LINHA
 		System.out.print("Informe uma quantia para sacar: ");
 		double amount = sc.nextDouble();
-
-		//GAMBIARRA
-		String error = acc.validateWithdraw(amount);
-		if (error != null) {
-			System.out.println(error);
-		} else {
+		
+		try {
 			acc.withdraw(amount);
 			System.out.printf("Novo saldo: %.2f", acc.getBalance());
+		} catch (BusinessException e) {
+			System.out.println(e.getMessage());
 		}
-
 		sc.close();
 	}
 
--- 15 - Tratamento de exceções/exec_1/src/entities/Account.java
+++ 15 - Tratamento de exceções/exec_1/src/entities/Account.java
@@ -1,5 +1,7 @@
 package entities;
 
+import exceptions.BusinessException;
+
 public class Account {
 
 	private Integer number;
@@ -56,18 +58,17 @@ public class Account {
 	}
 
 	public void withdraw(double amount) {
+		validateWithdraw(amount);
 		balance -= amount;
 	}
-	
-	// GAMBIARRA 
-	public String validateWithdraw(double amount) {
+
+	private void validateWithdraw(double amount) {
 		if (amount > getWithdrawLimit()) {
-			return "Erro de saque: A quantia excede o limite de saque";
+			throw new BusinessException("Erro de saque: A quantia excede o limite de saque");
 		}
 		if (amount > getBalance()) {
-			return "Erro de saque: Saldo insuficiente";
+			throw new BusinessException("Erro de saque: Saldo insuficiente");
 		}
-		return null;
 	}
 
 }

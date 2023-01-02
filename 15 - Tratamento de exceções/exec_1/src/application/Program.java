1c599516e0e75f87dfe9affde9e216cd7d4bf0d6
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-02 14:33:59 -0300

    Exercício Tratamento de exceções

---
+++ 15 - Tratamento de exceções/exec_1/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Informe os dados da conta");
+		System.out.println("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine(); // BUFFER ...
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println(); // QUEBRA DE LINHA
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		acc.withdraw(amount);
+		System.out.printf("Novo saldo: %.2f", acc.getBalance());
+		
+		sc.close();
+	}
+
+}
---
+++ 15 - Tratamento de exceções/exec_1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kaioh
+ *
+ */
+module exec_1 {
+}
